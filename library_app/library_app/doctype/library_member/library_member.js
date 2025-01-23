ccfe900acf2a9e3708e71d4c0438b9f1e3f49b17
Manya Girdhar <manyagirdhar@192.168.1.18>
2025-01-23 15:25:22 +0530

    Library member fucntionalities added (.py, .js)

--- library_app/library_app/doctype/library_member/library_member.js
+++ library_app/library_app/doctype/library_member/library_member.js
@@ -6,3 +6,44 @@
 
 // 	},
 // });
+
+frappe.ui.form.on('Library Member', {
+    before_save: function (frm) {
+        if (!frm.confirmation_shown) {
+            frappe.confirm(
+                'Are you sure you want to save the details of this new Library Member?',
+                () => {
+                    // If confirmed, set a flag and proceed with saving
+                    frm.confirmation_shown = true; // Prevent re-triggering the dialog
+                    frm.save(); // Manually trigger the save process
+                    frappe.msgprint(__('Library Member created successfully!')); // Debug success message
+                },
+                () => {
+                    // If canceled, prevent saving
+                    frappe.msgprint(__('Save operation has been canceled.')); // Debug cancel message
+                    frappe.validated = false; // Block save
+                }
+            );
+            frappe.validated = false; // Block the save temporarily
+        } else {
+            // Reset the flag after confirmation
+            frm.confirmation_shown = false;
+        }
+    }
+});
+
+
+frappe.ui.form.on('Library Member', {
+    refresh: function (frm) {
+        frm.add_custom_button('Create Membership', () => {
+            frappe.new_doc('Library Membership', {
+                library_member: frm.doc.name
+            })
+        })
+        frm.add_custom_button('Create Transaction', () => {
+            frappe.new_doc('Library Transaction', {
+                library_member: frm.doc.name
+            })
+        })
+    }
+});
---
+++ library_app/library_app/notification/new_member_notification/new_member_notification.md
@@ -0,0 +1,1 @@
+<p>A new member {doc.first_name} is created in library management system</p>
