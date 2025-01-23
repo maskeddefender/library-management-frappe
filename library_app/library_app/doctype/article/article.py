4fff1b89e1fc5ebde94548c57570813e074ed70c
yashvisharma2903 <co21370@ccet.ac.in>
2025-01-23 14:50:44 +0530

    templates commit

--- library_app/library_app/doctype/article/article.py
+++ library_app/library_app/doctype/article/article.py
@@ -2,8 +2,26 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
+import frappe  # Place imports at the top of the file
 
 
-class Article(Document):
-	pass
+class Article(WebsiteGenerator):
+    pass  # No need to define unnecessary methods if not required
+
+
+# Utility function to fetch articles
+def get_articles():
+    # Fetch articles with required fields
+    articles = frappe.get_all(
+        "Article",
+        filters={"is_published": 1},  # Only fetch published articles
+        fields=["article_name", "image", "author", "route"]  # Fetch necessary fields
+    )
+    return articles
+
+@frappe.whitelist()
+def render_page():
+    articles = get_articles()
+    return frappe.render_template("templates/includes/article.row.html", {"articles": articles})
+
